ab90e7e1f6e785c1569ed8848be95d22f25323d9
danielamitay <hello@danielamitay.com>
2017-08-14 00:27:18 -0400

    Request manager improvements

--- Source/API/KUSRequestManager.m
+++ Source/API/KUSRequestManager.m
@@ -16,6 +16,7 @@
 
 @property (nonatomic, strong, readonly) NSString *baseUrlString;
 @property (nonatomic, strong, readonly) NSURLSession *urlSession;
+@property (nonatomic, strong, readonly) dispatch_queue_t queue;
 
 @end
 
@@ -31,9 +32,13 @@
 
         _baseUrlString = KUSBaseUrlStringFromOrgName(_userSession.orgName);
 
+        _queue = dispatch_queue_create("com.kustomer.request-manager", nil);
+
+        NSOperationQueue *operationQueue = [[NSOperationQueue alloc] init];
+        operationQueue.underlyingQueue = _queue;
         NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
         [configuration setTimeoutIntervalForRequest:15.0];
-        _urlSession = [NSURLSession sessionWithConfiguration:configuration delegate:nil delegateQueue:nil];
+        _urlSession = [NSURLSession sessionWithConfiguration:configuration delegate:nil delegateQueue:operationQueue];
     }
     return self;
 }
@@ -93,65 +98,44 @@
          additionalHeaders:(NSDictionary *)additionalHeaders
                 completion:(KUSRequestCompletion)completion
 {
-    if (type == KUSRequestTypeGet) {
-        NSURLComponents *urlComponents = [NSURLComponents componentsWithURL:URL resolvingAgainstBaseURL:NO];
-        NSMutableArray<NSURLQueryItem *> *queryItems = [[NSMutableArray alloc] initWithCapacity:params.count];
-        for (NSString *key in params) {
-            id value = params[key];
-            NSString *valueString = nil;
-            if ([value isKindOfClass:[NSString class]]) {
-                valueString = (NSString *)value;
-            } else {
-                valueString = [NSString stringWithFormat:@"%@", value];
+    dispatch_async(self.queue, ^{
+        NSURL *finalURL = (type == KUSRequestTypeGet ? KUSURLFromURLAndQueryParams(URL, params) : URL);
+        NSMutableURLRequest *urlRequest = [[NSMutableURLRequest alloc] initWithURL:finalURL];
+        [urlRequest setHTTPMethod:KUSRequestTypeToString(type)];
+        [urlRequest setValue:@"kustomer" forHTTPHeaderField:@"X-Kustomer"];
+        if (type != KUSRequestTypeGet) {
+            KUSAttachJSONBodyToRequest(urlRequest, params);
+        }
+
+        /*
+        if (authenticated && self.trackingToken) {
+            [urlRequest setValue:self.trackingToken forHTTPHeaderField:kKustomerTrackingTokenHeaderKey];
+        }
+        */
+
+        void (^safeComplete)(NSError *, NSDictionary *) = ^void(NSError *error, NSDictionary *response) {
+            if (completion) {
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    if (error) {
+                        completion(error, nil);
+                    } else {
+                        completion(nil, response);
+                    }
+                });
             }
-            NSURLQueryItem *queryItem = [NSURLQueryItem queryItemWithName:key value:valueString];
-            [queryItems addObject:queryItem];
-        }
-        urlComponents.queryItems = queryItems;
-        URL = urlComponents.URL;
-    }
-
-    NSMutableURLRequest *urlRequest = [[NSMutableURLRequest alloc] initWithURL:URL];
-    [urlRequest setHTTPMethod:KUSRequestTypeToString(type)];
-
-    if (params && type != KUSRequestTypeGet) {
-        NSData *jsonData = [NSJSONSerialization dataWithJSONObject:params options:kNilOptions error:NULL];
-        [urlRequest setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
-        NSString *contentLength = [NSString stringWithFormat:@"%lu", (unsigned long)jsonData.length];
-        [urlRequest setValue:contentLength forHTTPHeaderField:@"Content-Length"];
-        [urlRequest setHTTPBody:jsonData];
-    }
-
-    [urlRequest setValue:@"kustomer" forHTTPHeaderField:@"X-Kustomer"];
-
-    /*
-    if (authenticated && self.trackingToken) {
-        [urlRequest setValue:self.trackingToken forHTTPHeaderField:kKustomerTrackingTokenHeaderKey];
-    }
-    */
-
-    void (^safeComplete)(NSError *, NSDictionary *) = ^void(NSError *error, NSDictionary *response) {
-        if (completion) {
-            dispatch_async(dispatch_get_main_queue(), ^{
-                if (error) {
-                    completion(error, nil);
-                } else {
-                    completion(nil, response);
-                }
-            });
-        }
-    };
-    void (^responseBlock)(NSData *, NSURLResponse *, NSError *) = ^void(NSData *data, NSURLResponse *response, NSError *error) {
-        if (error) {
-            safeComplete(error, nil);
-            return;
-        }
-        NSError *jsonError;
-        NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:&jsonError];
-        safeComplete(jsonError, json);
-    };
-    NSURLSessionDataTask *dataTask = [_urlSession dataTaskWithRequest:urlRequest completionHandler:responseBlock];
-    [dataTask resume];
+        };
+        void (^responseBlock)(NSData *, NSURLResponse *, NSError *) = ^void(NSData *data, NSURLResponse *response, NSError *error) {
+            if (error) {
+                safeComplete(error, nil);
+                return;
+            }
+            NSError *jsonError;
+            NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:&jsonError];
+            safeComplete(jsonError, json);
+        };
+        NSURLSessionDataTask *dataTask = [_urlSession dataTaskWithRequest:urlRequest completionHandler:responseBlock];
+        [dataTask resume];
+    });
 }
 
 #pragma mark - Helper methods
@@ -177,6 +161,40 @@ static NSString *KUSBaseUrlStringFromOrgName(NSString *orgName)
     NSDictionary<NSString *, NSString *> *environment = [[NSProcessInfo processInfo] environment];
     NSString *baseDomain = environment[@"KUSTOMER_BASE_DOMAIN"] ?: @"kustomerapp.com";
     return [NSString stringWithFormat:@"https://%@.api.%@", orgName, baseDomain];
+}
+
+static NSURL *KUSURLFromURLAndQueryParams(NSURL *URL, NSDictionary<NSString *, id> *params)
+{
+    if (params.count < 1) {
+        return URL;
+    }
+
+    NSURLComponents *urlComponents = [NSURLComponents componentsWithURL:URL resolvingAgainstBaseURL:NO];
+    NSMutableArray<NSURLQueryItem *> *queryItems = [[NSMutableArray alloc] initWithCapacity:params.count];
+    for (NSString *key in params) {
+        id value = params[key];
+        NSString *valueString = nil;
+        if ([value isKindOfClass:[NSString class]]) {
+            valueString = (NSString *)value;
+        } else {
+            valueString = [NSString stringWithFormat:@"%@", value];
+        }
+        NSURLQueryItem *queryItem = [NSURLQueryItem queryItemWithName:key value:valueString];
+        [queryItems addObject:queryItem];
+    }
+    urlComponents.queryItems = queryItems;
+    return urlComponents.URL;
+}
+
+static void KUSAttachJSONBodyToRequest(NSMutableURLRequest *mutableURLRequest, NSDictionary<NSString *, id> *params)
+{
+    if (params.count > 0) {
+        NSData *jsonData = [NSJSONSerialization dataWithJSONObject:params options:kNilOptions error:NULL];
+        [mutableURLRequest setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+        NSString *contentLength = [NSString stringWithFormat:@"%lu", (unsigned long)jsonData.length];
+        [mutableURLRequest setValue:contentLength forHTTPHeaderField:@"Content-Length"];
+        [mutableURLRequest setHTTPBody:jsonData];
+    }
 }
 
 @end
