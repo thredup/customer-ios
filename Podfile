492ae2b85e5845b6012e1c4940ddd1ad8540658a
danielamitay <hello@danielamitay.com>
2017-08-27 11:33:25 -0400

    Add TTTAttributedLabel Pod

--- Podfile
+++ Podfile
@@ -6,6 +6,7 @@ target 'Kustomer' do
   pod 'libPusher', '~> 1.6.3'
   pod 'TSMarkdownParser', '~> 2.1.3'
   pod 'SDWebImage', '~> 4.1.0'
+  pod 'TTTAttributedLabel', '~> 2.0.0'
 
   target 'KustomerTests' do
     inherit! :search_paths
--- Pods/Target Support Files/Pods-KustomerTests/Pods-KustomerTests-frameworks.sh
+++ Pods/Target Support Files/Pods-KustomerTests/Pods-KustomerTests-frameworks.sh
@@ -92,12 +92,14 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/SDWebImage/SDWebImage.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SocketRocket/SocketRocket.framework"
   install_framework "$BUILT_PRODUCTS_DIR/TSMarkdownParser/TSMarkdownParser.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/TTTAttributedLabel/TTTAttributedLabel.framework"
   install_framework "$BUILT_PRODUCTS_DIR/libPusher/Pusher.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/SDWebImage/SDWebImage.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SocketRocket/SocketRocket.framework"
   install_framework "$BUILT_PRODUCTS_DIR/TSMarkdownParser/TSMarkdownParser.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/TTTAttributedLabel/TTTAttributedLabel.framework"
   install_framework "$BUILT_PRODUCTS_DIR/libPusher/Pusher.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
